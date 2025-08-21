d1cb08af65e2015c06097827c3b2af977044bf1d
JoSEpHLJ2 <3058524456@qq.com>
2025-08-21 14:42:55 +1000

    Update test1.1.py

--- test1.1.py
+++ test1.1.py
@@ -1,26 +1,24 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("PyTorch Version:", torch.__version__)
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# grid for computing image, subdivide the space
+# grid for computing image
 X, Y = np.mgrid[-4.0:4:0.01, -4.0:4:0.01]
 
 # load into PyTorch tensors
-x = torch.Tensor(X)
-y = torch.Tensor(Y)
+x = torch.Tensor(X).to(device)
+y = torch.Tensor(Y).to(device)
 
-# transfer to the GPU device
-x = x.to(device)
-y = y.to(device)
-# Compute Gaussian
-z = torch.exp(-(x**2+y**2)/2.0)
-#plot
-import matplotlib.pyplot as plt
-plt.imshow(z.cpu().numpy())#Updated!
+# Compute sine pattern (change here)
+z = torch.sin(x) * torch.sin(y)
+
+# plot
+plt.imshow(z.cpu().numpy(), cmap='viridis')  # 可以换 cmap 看效果
 plt.tight_layout()
 plt.show()
 plt.savefig("test1.1.png", dpi=300, bbox_inches="tight")
